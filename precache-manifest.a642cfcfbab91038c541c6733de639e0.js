fe52ddee3435216e0beef427c162dbf35a1755f8
CN138 <cn138@CN0138.local>
2019-09-11 14:59:49 +0100

    Updates

---
+++ precache-manifest.a642cfcfbab91038c541c6733de639e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f59d2e986a27bc7051d86d4279c8b4",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "2bdd72fe61fe849b27de",
+    "url": "/calculator/static/css/main.bf2c2525.chunk.css"
+  },
+  {
+    "revision": "498308cdfe445072f369",
+    "url": "/calculator/static/js/2.62b03613.chunk.js"
+  },
+  {
+    "revision": "2bdd72fe61fe849b27de",
+    "url": "/calculator/static/js/main.e7b2a57e.chunk.js"
+  },
+  {
+    "revision": "f201155ca5381ed642cd",
+    "url": "/calculator/static/js/runtime~main.c365a1b3.js"
+  }
+]);
